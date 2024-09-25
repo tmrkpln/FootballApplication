6ecd63c504e829cd4028952118b48fc4254fd0b1
tmrkpln <131858178+tmrkpln@users.noreply.github.com>
2024-09-25 16:49:34 +0300

    ..

--- BusinessLayer/obj/Debug/net8.0/BusinessLayer.AssemblyInfo.cs
+++ BusinessLayer/obj/Debug/net8.0/BusinessLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+031a07a4cf749fa6ccd6b33ac5909dc4859188f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
